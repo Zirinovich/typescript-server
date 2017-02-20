6dc3388e3bfa0c5a98c8fd94336163bb34b66886
grisha <gmamontov@alt-lan.ru>
2017-02-20 18:58:00 +0300

    webpack-dev-middleware

--- server.ts
+++
@@ -1,5 +0,0 @@
-//['.css', '.less', '.sass', '.ttf', '.woff', '.woff2'].forEach((ext) => require.extensions[ext] = () => {});
-
-import Server from './src/server';
-
-Server.start();
---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Link } from 'react-router';
+
+export const Header = () => (
+    <nav>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="about">About</Link></li>
+            <li><Link to="counter">Counter</Link></li>
+            <li><Link to="stars">Stars</Link></li>
+        </ul>
+    </nav>
+);
