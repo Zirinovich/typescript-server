347a6a50da6a99891c1d5ea273a5fffdad6ad6bd
grisha <gmamontov@alt-lan.ru>
2017-02-27 17:56:55 +0300

    webpack-dev-middleware

--- src/server.tsx
+++ src/server.tsx
@@ -11,6 +11,25 @@ import { App, Html } from './app/containers';
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackConfig = require('../config/webpack/dev');
+  const webpackCompiler = webpack(webpackConfig);
+
+  app.use(require('webpack-dev-middleware')(webpackCompiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: { colors: true },
+    noInfo: true,
+    hot: true,
+    inline: true,
+    lazy: false,
+    historyApiFallback: true,
+    quiet: true,
+  }));
+
+  app.use(require('webpack-hot-middleware')(webpackCompiler));
+}
+
 app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
 
 app.get('*', (req, res) => {
