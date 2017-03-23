9e5bbb6c4067f58d226b239ed0127187bcecc0a1
DEV-SUP\Euegene Zyrin <ezyrin@alt-lan.ru>
2017-03-23 17:51:05 +0300

    Memory leaks from 'express-session' FIX

--- src/server/expressSetup/ExpressSessionTuner.ts
+++ src/server/expressSetup/ExpressSessionTuner.ts
@@ -3,10 +3,13 @@ import session = require("express-session");
 
 export class ExpressSessionTuner {
     static Setup(app: Express) {
+        let MemoryStore = require('session-memory-store')(session);
         app.use(session({
+            name: 'x-session',
             secret: 'yaouyahanSecretWord',
             resave: false,
-            saveUninitialized: false
+            saveUninitialized: false,
+            store: new MemoryStore({expires: 60 * 60 * 4})
         }));
     }
 }
