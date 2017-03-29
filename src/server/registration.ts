e98abe34a15ca10e8dc09dc73658bd22e7ff7215
DEV-SUP\Euegene Zyrin <ezyrin@alt-lan.ru>
2017-03-29 11:28:32 +0300

    Правка синтаксической ошибки

--- src/server/registration.ts
+++ src/server/registration.ts
@@ -1,6 +1,6 @@
 import Ioc from '../shared/classes/ioc';
 import {PassportLocalStrategyMiddlewareFunctions} from './authenticationPassport/PassportLocalStrategyMiddlewareFunctions';
-import {UserStore} from './PostgeDatabase/UserStore';
+import {UserStore} from './PostgreDatabase/UserStore';
 
 Ioc.register("IAuthenticationMiddleware", true, new PassportLocalStrategyMiddlewareFunctions());
 Ioc.register("IUserStore", true, new UserStore());
