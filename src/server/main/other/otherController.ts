de903e5e26bc8cfee12a60917cef4454a336cfdf
Zirinovich <zhorzhmail@gmail.com>
2017-05-19 12:29:17 +0300

    +

--- src/server/main/other/otherController.ts
+++ src/server/main/other/otherController.ts
@@ -1,3 +1,4 @@
+/*
 import {router, AuthClaims} from "../../_engine/routers/router";
 import HTTP_STATUS_CODES from 'http-status-enum';
 import {authenticationMiddleware, usersLogic} from "../../registration";
@@ -5,4 +6,4 @@ router.get('/test2', (req, res) => {
     usersLogic.i++;
     usersLogic.test(10);
     res.status(HTTP_STATUS_CODES.OK).json(req.user);
-});
+});*/
