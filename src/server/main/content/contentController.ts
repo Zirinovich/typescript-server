3a3350d497a135bfef9945c6e0f9fd23bfa7bd5d
Zirinovich <zhorzhmail@gmail.com>
2017-05-29 18:10:25 +0300

    upload

--- src/server/main/content/contentController.ts
+++ src/server/main/content/contentController.ts
@@ -1,7 +1,14 @@
 import {router, AuthClaims} from "../../_engine/routers/router";
 import {contentLogic} from "../../registration";
+let multiparty = require('connect-multiparty');
 
-router.post('/main/users/addchangecontent', async(req, res) => {
+router.post('/main/content/addchangecontent', async(req, res) => {
+    const requestData = req.body;
+    let id = await contentLogic.addChangeContentAsync(requestData);
+});
+
+router.post('/main/content/upload', multiparty({uploadDir: './uploaded'}));
+router.post('/main/content/upload', async(req, res) => {
     const requestData = req.body;
     let id = await contentLogic.addChangeContentAsync(requestData);
 });
